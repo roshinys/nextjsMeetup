d4acd747db8d2bfbae0a9133700e94e26d38a63a
roshinys <roshinchaya@gmail.com>
2023-08-04 12:29:28 +0530

    added meetup form and details routings

---
+++ app/[meetupId]/page.jsx
@@ -0,0 +1,13 @@
+import MeetUpDetail from "@/components/meetups/MeetUpDetail";
+
+export default function MeetUpDetails() {
+  return (
+    <MeetUpDetail
+      id="m1"
+      title="A First Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
+      address="Some address 5, 12345 Some City"
+      description="This is a first meetup!"
+    />
+  );
+}
---
+++ app/new-meetup/page.jsx
@@ -0,0 +1,9 @@
+"use client";
+import NewMeetupForm from "@/components/meetups/NewMeetupForm";
+
+export default function NewMeetup() {
+  function addMeetupHandler(enteredMeetup) {
+    console.log(enteredMeetup);
+  }
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+}
