1918dddf1ee9d72ee0faedf004ba2b8368120802
roshinys <roshinchaya@gmail.com>
2023-08-05 13:16:46 +0530

    fixed image remdering

--- components/meetups/MeetUpDetail.js
+++ components/meetups/MeetUpDetail.js
@@ -1,11 +1,10 @@
 import React from "react";
 import classes from "./MeetUpDetail.module.css";
-import Image from "next/image";
 
 function MeetUpDetail(props) {
   return (
     <section className={classes.detail}>
-      <Image src={props.image} alt={props.title} width={100} height={100} />
+      <img src={props.image} alt={props.title} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
