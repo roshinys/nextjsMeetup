8ff6e247d434fa2359981c439de915ad7ea5a644
roshinys <roshinchaya@gmail.com>
2023-08-05 13:05:46 +0530

    refactore code

--- components/meetups/MeetUpDetail.js
+++ components/meetups/MeetUpDetail.js
@@ -1,10 +1,11 @@
 import React from "react";
 import classes from "./MeetUpDetail.module.css";
+import Image from "next/image";
 
 function MeetUpDetail(props) {
   return (
     <section className={classes.detail}>
-      <img src={props.image} alt={props.title} />
+      <Image src={props.image} alt={props.title} width={100} height={100} />
       <h1>{props.title}</h1>
       <address>{props.address}</address>
       <p>{props.description}</p>
